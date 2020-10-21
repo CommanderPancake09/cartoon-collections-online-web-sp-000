dd5fae090fb57880b7c62dc57f019ad978598177
Tyler Young <tyoung900@gmail.com>
2020-10-21 21:43:21 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -32,7 +32,16 @@ def long_planeteer_calls(planeteer_calls)
   planeteer_calls.any? { |call| call.length > 4  }
 end
 
-def find_the_cheese# code an argument here
-  # the array below is here to help
+long_planeteer_calls(planeteer_calls)
+
+potentially_cheesey_items = %w[umbrella spinach cheddar helicopter]
+
+def find_the_cheese(potentially_cheesey_items)
   cheese_types = ["cheddar", "gouda", "camembert"]
+
+  potentially_cheesey_items.find do |maybe_cheese|
+    cheeses.include?(maybe_cheese)
+  end
 end
+
+find_the_cheese(potentially_cheesey_items)
